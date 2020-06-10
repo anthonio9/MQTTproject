b3ce2c27660f393cfe8966cef0017661b037ba5a
natalia <natalia@inuits.io>
2020-06-10 17:53:33 +0200

    MQTTClient prepared()

--- src/mqtt_project/mqtt_base.hpp
+++ src/mqtt_project/mqtt_base.hpp
@@ -17,15 +17,13 @@
 #include <vector>
 #include <unordered_map>
 
-
-
 // CLIENT TYPES
 const int PUBLISHER = 0;
 const int SUBSCRIBER = 1;
 
 // MSG TYPES
 const int INIT = 11;
-const int MSG = 12;
+const int DATA = 12;
 
 // BROKER DEFAULT PORT
 const int BROKER_PORT = 7733;
@@ -36,15 +34,15 @@ const int BROKER_PORT2 = 7734;
 #define BUFFSIZE 2048
 
 // COMMON MESSAGE
-struct mqtt_msg {
-	int cli_type;			// constant int SUBSCRIBER or PUBLISHER.
-	int msg_type;			// constant int INIT or MSG
-	char topic[20]; 		// name of subscribed topic.
-	size_t topic_len; 		// size of topic.
-	char data[100];			// array containing message.
-	size_t data_len;		// length of message.
+struct mqtt_msg
+{
+	int cli_type;	  // constant int SUBSCRIBER or PUBLISHER.
+	int msg_type;	  // constant int INIT or MSG
+	char topic[20];	  // name of subscribed topic.
+	size_t topic_len; // size of topic.
+	char data[100];	  // array containing message.
+	size_t data_len;  // length of message.
 };
-
 
 class MQTTBroker
 {
@@ -55,7 +53,8 @@ protected:
 	struct sctp_event_subscribe evnts;
 	char readbuf[BUFFSIZE];
 	size_t rd_sz;
-	std::unordered_map<std::string, std::vector<struct sctp_sndrcvinfo>> topics; 
+	std::unordered_map<std::string, std::vector<struct sctp_sndrcvinfo>> topics;
+
 public:
 	// Default constructor
 	MQTTBroker();
@@ -68,7 +67,34 @@ protected:
 	int prepare_server();
 	int set_options();
 	int listen_msg();
+};
 
+class MQTTClient
+{
+protected:
+	int sock_fd, service, af_family, addr_len;
+	struct sockaddr_in local_addr, broker_addr;
+	struct sctp_sndrcvinfo sri;
+	struct sctp_event_subscribe evnts;
+	struct mqtt_msg msg;
+	char readbuf[BUFFSIZE];
+	size_t rd_sz;
+	char broker_ip[100];
+
+public:
+	// Default constructor
+	MQTTClient();
+
+	// Parametrized constructor
+	MQTTClient(char *_broker_ip, size_t ip_len, int _service, int _af_family);
+
+protected:
+	int prepare_client();
+	int set_options();
+	int recv_mqtt();
+
+public:
+	int send_mqtt();
 };
 
 #endif
